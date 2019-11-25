352a83b3550ad42bcff36d21283b0843ada26101
ALaks96 <arek.laks@gmail.com>
2019-11-25 17:56:59 +0100

    progress on krypton!

--- KRYPTON/krypton.sh
+++ KRYPTON/krypton.sh
@@ -22,3 +22,22 @@ cat found3
 # Go to a frequency analysis website to check occurences of characters and compare. This will be the key to substitute the characters in krypton 3.
 cat krypton 3 | tr ABCDEFGHIJKLMNOPQRSTUVWXYZ BOIHGKNQVTWYURXZAJEMSLDFPC
 
+# krypton level 4 -> 5
+ls -al
+cat HINT
+cat found1
+cat krypton5
+# We use a viginère decipher program to get key, hint tells us key is of length 6 which easily gives us the password for the next level
+
+# krypton level 5 -> level 6
+ls -al
+cat found1
+cat found2
+cat found3
+# First we need to find the highest probabilty of length of key word so we try analysing with the same site as before. We see that length of 9 is the most recurring result.
+# Then we register every key cracked from the cipher texts: yeylergth, koylenvtq, yeylergth.
+# same key comes up twice. Trying out gives us the password.
+cat krypton6 # BELOS Z == RANDO M
+
+
+
